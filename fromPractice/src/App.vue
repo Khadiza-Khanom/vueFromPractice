e95fe414778c20861eb086d49999b5264cafb0ce
unknown <khadizakhanommeem@gmail.com>
2023-08-01 14:35:50 +0600

    checkbox

--- fromPractice/src/App.vue
+++ fromPractice/src/App.vue
@@ -1,34 +1,29 @@
 <script setup>
-import {ref,reactive} from 'vue'
-
-const person=reactive({
-  fname:'Sheikh',
-  lname:'Yusuf',
-  age:1.5,
-  gender: 'boy'
-}
-)
-
+import{ref} from 'vue'
+const checkValue= ref(true)
 </script>
 
 <template>
   <section class="mx-auto container">
     <h1 class="text-2xl mb-10">Vue Form</h1>
+    <p class="mb-10"></p>
+    <label for="">Display Random Image</label>
+    <input class="ml-2" type="checkbox"  v-model="checkValue" />
 
-    <p class="mb-10">
-    {{ person }}
-    </p>
+    <img
+    v-show="checkValue"
+      class="mt-10 mx-auto w-[500px]"
+      :src="`https://source.unsplash.com/random?version=${ Math.random()}`"
+      alt=""  />
+      
+    <!-- <img
+    v-if="checkValue"
+      class="mt-10 mx-auto w-[500px]"
+      :src="`https://source.unsplash.com/random?version=${ Math.random()}`"
+      alt=""  /> -->
 
-    <div class="flex flex-col mb-5 text-left" v-for="value,key,index in person" :key="key" >
-      <label for="name">{{key}}:  </label>
-      <input type="text" class="border border-gray-300 rounded-md p-2"  v-model="person[key]" />
-    </div>
+    
 
   </section>
 </template>
-
-
-
-
-<style scoped>
-</style>
+<style scoped></style>
